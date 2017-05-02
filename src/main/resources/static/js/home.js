516f5b8a276dce66a31f7652289e37b75ce44a0e
Scott <824260107@qq.com>
2017-05-02 23:58:33 +0800

    热部署

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -3,7 +3,6 @@
  */
 $("#sub").click(function () {
     $.get("http://localhost:8080/greet?name=" + $("#txt").val()).then(function() {
-        alert($("#txt").val());
         location.href = "http://localhost:8080/greet?name=" + $("#txt").val();
     });
 });
