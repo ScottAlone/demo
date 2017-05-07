516f68ca37b0236d9be4d3f974c0739372360b98
1452719 <2357091486@qq.com>
2017-05-07 15:15:29 +0800

    login & register ui

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,8 @@
+function register() {
+	document.getElementById("login").style.display="none";
+	document.getElementById("register").style.display="inline";
+}
+function login() {
+	document.getElementById("login").style.display="inline";
+	document.getElementById("register").style.display="none";
+}
